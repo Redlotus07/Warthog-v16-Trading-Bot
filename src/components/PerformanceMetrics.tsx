23cc2eeccdfdab0ab2f64169df23c9306c20f2a4
Redlotus07 <officialgulliver0@gmail.com>
2024-10-31 04:26:34 +0100

    Update PerformanceMetrics.tsx

--- src/components/PerformanceMetrics.tsx
+++ src/components/PerformanceMetrics.tsx
@@ -1,3 +1,4 @@
+// src/components/PerformanceMetrics.tsx
 import React, { useState, useEffect } from 'react';
 import { LineChart, Line, XAxis, YAxis, CartesianGrid, Tooltip, ResponsiveContainer } from 'recharts';
 import axios from 'axios';
